3d5f383b2198edcf8531fd13d473cc791bd080fd
Sergei Munovarov <serhuz@ymail.com>
2017-12-10 02:07:08 +0200

    Add rule to remove DB before tests

---
+++ app/src/androidTest/java/ua/sergeimunovarov/tcalc/ClearDatabaseRule.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright © Sergei Munovarov. All rights reserved.
+ * See LICENCE.txt for license details.
+ */
+
+package ua.sergeimunovarov.tcalc;
+
+import android.support.test.InstrumentationRegistry;
+
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+
+
+public class ClearDatabaseRule implements TestRule {
+
+    private final String mDatabaseName;
+
+
+    public ClearDatabaseRule(String databaseName) {
+        mDatabaseName = databaseName;
+    }
+
+
+    @Override
+    public Statement apply(Statement base, Description description) {
+        return new Statement() {
+            @Override
+            public void evaluate() throws Throwable {
+                InstrumentationRegistry.getTargetContext().deleteDatabase(mDatabaseName);
+                base.evaluate();
+            }
+        };
+    }
+}

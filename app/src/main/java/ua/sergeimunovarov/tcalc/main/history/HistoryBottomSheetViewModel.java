9502eee314d39dd9d5a3cd02a0330146a85ca4e3
Sergei Munovarov <serhuz@ymail.com>
2018-03-02 00:24:06 +0200

    Use view model providers

--- app/src/main/java/ua/sergeimunovarov/tcalc/main/history/HistoryBottomSheetViewModel.java
+++ app/src/main/java/ua/sergeimunovarov/tcalc/main/history/HistoryBottomSheetViewModel.java
@@ -6,14 +6,14 @@
 package ua.sergeimunovarov.tcalc.main.history;
 
 import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModelProvider;
 import android.databinding.ObservableArrayList;
 import android.databinding.ObservableList;
+import android.support.annotation.NonNull;
 
 import java.util.List;
 
-import javax.inject.Inject;
-
-import ua.sergeimunovarov.tcalc.Application;
 import ua.sergeimunovarov.tcalc.main.BaseObservableViewModel;
 import ua.sergeimunovarov.tcalc.main.history.db.Entry;
 import ua.sergeimunovarov.tcalc.main.history.db.EntryDao;
@@ -24,12 +24,28 @@ public class HistoryBottomSheetViewModel extends BaseObservableViewModel {
     public final ObservableList<Entry> mEntries = new ObservableArrayList<>();
     public final LiveData<List<Entry>> mLiveHistoryItems;
 
-    @Inject
-    EntryDao mDao;
+
+    @SuppressWarnings("WeakerAccess")
+    public HistoryBottomSheetViewModel(@NonNull EntryDao dao) {
+        mLiveHistoryItems = dao.getAll();
+    }
 
 
-    public HistoryBottomSheetViewModel() {
-        Application.getAppComponent().inject(this);
-        mLiveHistoryItems = mDao.getAll();
+    public static class Factory implements ViewModelProvider.Factory {
+
+        private final EntryDao mDao;
+
+
+        public Factory(@NonNull EntryDao dao) {
+            mDao = dao;
+        }
+
+
+        @SuppressWarnings("unchecked")
+        @NonNull
+        @Override
+        public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+            return (T) new HistoryBottomSheetViewModel(mDao);
+        }
     }
 }

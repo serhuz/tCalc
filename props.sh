c2540be9a17c038da9f61bd0b8aec01607bead2e
Sergei Munovarov <serhuz@ymail.com>
2018-09-29 14:42:58 +0300

    Fix props.sh

--- props.sh
+++ props.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-GRADLE_PROPS = ${HOME}/tcalc/gradle.properties
+GRADLE_PROPS=${HOME}/tcalc/gradle.properties
 export GRADLE_PROPS
 
 if [ ! -f "$GRADLE_PROPS" ]; then
 
-    touch ${GRADLE_PROPS}
+    touch -f ${GRADLE_PROPS} &&
 
-    echo "android.enableJetifier=true" >> ${GRADLE_PROPS}
+    echo "android.enableJetifier=true" >> ${GRADLE_PROPS} &&
     echo "android.useAndroidX=true" >> ${GRADLE_PROPS}
 
 fi
